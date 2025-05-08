d6fb2c47c17c97799f505fc6cd590080685f2008
Anoop Kumar A <92391766+AnoopkumarA@users.noreply.github.com>
2025-05-08 20:33:32 +0530

    Update projectsection.tsx

--- src/pages/projectsection.tsx
+++ src/pages/projectsection.tsx
@@ -1,11 +1,28 @@
-
+import { useState, useEffect } from 'react';
 
 const ProjectSection = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+    
+    // Initial check
+    checkIfMobile();
+    
+    // Add event listener
+    window.addEventListener('resize', checkIfMobile);
+    
+    // Cleanup
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
+
   return (
-    <div className="pt-20">
-      <div className="flex flex-col md:flex-row gap-6 px-20 p-6 top-16 relative bg-white">
+    <div className="pt-12 md:pt-20">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-6 px-4 md:px-20 p-3 md:p-6 top-16 relative bg-white">
         {/* Left Image */}
-        <div className="w-full mx-auto max-w-[48rem] h-[29rem] md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto md:max-w-[48rem] h-[18rem] md:h-[29rem] md:w-1/2 overflow-hidden">
           <img
             src="/10.png"
             alt="Swing Area"
@@ -14,7 +31,7 @@ const ProjectSection = () => {
         </div>
 
         {/* Right Image */}
-        <div className="w-full mx-auto max-w-[48rem] h-[28.9rem] md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[48rem] h-[18rem] md:h-[28.9rem] md:w-1/2 overflow-hidden">
           <img
             src="/11.png"
             alt="Ceiling View"
@@ -23,10 +40,9 @@ const ProjectSection = () => {
         </div>
       </div>
 
-
-      <div className="flex flex-col md:flex-row px-18 mt-32 p-6 bg-white">
+      <div className="flex flex-col md:flex-row px-4 md:px-18 mt-16 md:mt-32 p-3 md:p-6 bg-white">
         {/* Left Image */}
-        <div className="w-full mx-auto max-w-[24rem] h-auto md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[24rem] h-[18rem] md:h-auto md:w-1/2 overflow-hidden mb-4 md:mb-0">
           <img
             src="/12.png"
             alt="Swing Area"
@@ -35,7 +51,7 @@ const ProjectSection = () => {
         </div>
 
         {/* Right Image */}
-        <div className="w-full mx-auto max-w-[29rem] h-auto md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[29rem] h-[18rem] md:h-auto md:w-1/2 overflow-hidden mb-4 md:mb-0">
           <img
             src="/13.png"
             alt="Ceiling View"
@@ -43,7 +59,7 @@ const ProjectSection = () => {
           />
         </div>
 
-        <div className="w-full mx-auto max-w-[29rem] h-auto md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[29rem] h-[18rem] md:h-auto md:w-1/2 overflow-hidden">
           <img
             src="/14.png"
             alt="Ceiling View"
@@ -52,9 +68,9 @@ const ProjectSection = () => {
         </div>
       </div>
 
-      <div className="flex flex-col md:flex-row gap-6 px-20 p-6  relative bg-white">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-6 px-4 md:px-20 p-3 md:p-6 relative bg-white">
         {/* Left Image */}
-        <div className="w-full mx-auto max-w-[48rem] h-[29rem] md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[48rem] h-[18rem] md:h-[29rem] md:w-1/2 overflow-hidden">
           <img
             src="/16.png"
             alt="Swing Area"
@@ -63,7 +79,7 @@ const ProjectSection = () => {
         </div>
 
         {/* Right Image */}
-        <div className="w-full mx-auto max-w-[48rem] h-[29rem] md:w-1/2 overflow-hidden">
+        <div className="w-full mx-auto max-w-[48rem] h-[18rem] md:h-[29rem] md:w-1/2 overflow-hidden">
           <img
             src="/15.png"
             alt="Ceiling View"
